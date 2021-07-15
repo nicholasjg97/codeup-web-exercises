8ddbd44bfb0145228f85cc7360bfd8b2e2555d33
Nicholas Gaytan <nicholas.j.gaytan97@gmail.com>
2021-07-14 21:03:05 -0500

    finished while loop js exercise

---
+++ js/function-writing-bonuses.js
@@ -0,0 +1,71 @@
+function isOdd(number) {
+    if (number % 2 !== 0) {
+        return true;
+    }
+}
+
+function isEven(number) {
+    if (number % 2 === 0) {
+        return true;
+    }
+}
+
+function identity(input) {
+    return input === input;
+}
+
+function isFive(input) {
+    return input = 5;
+}
+
+function addFive(input) {
+    return input + 5;
+}
+
+function isMultipleOfFive(input) {
+    if (input % 5 === 0) {
+        return true;
+    }
+}
+
+function isThree(input) {
+    return input = 3;
+}
+
+function isMultipleOfThree(input) {
+    if (input % 3 === 0) {
+        return true;
+    }
+}
+
+function isMultipleOfThreeAndFive(input) {
+    if (input % 3 ===0 && input % 5 === 0) {
+        return true;
+    }
+}
+
+function isMultipleOf(target, n) {
+    if (target % n === 0) {
+        return true;
+    }
+}
+
+function isTrue(boolean) {
+    return true;
+}
+
+function isFalse(boolean) {
+    return false;
+}
+
+function isTruthy(input) {
+
+}
+
+function isFalsy(input) {
+
+}
+
+function isVowel(letter) {
+
+}
---
+++ js/loops-lecture.js
@@ -0,0 +1,64 @@
+// var pizzaSlices = 4;
+//
+// while (pizzaSlices > 0) {
+//     console.log("i'll have a slice");
+//     pizzaSlices = pizzaSlices - 1;
+//     console.log("Now there's " + pizzaSlices + " slices left");
+// }
+//
+// console.log("Is it already all eaten?  :(");
+
+// var counter = 0;
+// while (counter <= 100) {
+//     console.log(counter);
+//     counter++;
+// }
+
+// var counter = 100;
+// while (counter >= 0) {
+//     console.log(counter);
+//     counter--;
+// }
+
+// var number = 5
+// while (number < 200) {
+//     console.log(number);
+//     number *= 2 // number = number * 2
+// }
+
+// pseudocode
+// Ask user for input -- store user unput in a variable
+// Declare a variable to hold running total
+// Add user input price of item to a running total
+// Loop back and ask the user for a price of next item
+// I need some way for user to break out of loop --
+// ?? stop or end or some other way for them to end the program
+// Tell the user how to end the program
+//?? Feature: warn the user when they reach the limit
+//?? When do you get free shipping?
+
+
+// accumulator variable: var total
+// var total = 0;
+//
+// var priceOfItem;
+//
+// while (true) {
+//     var userInput = prompt("Enter the price of your item: " + "\n " + "Enter STOP to end program");
+//     // sentinel total: STOP
+//     if (userInput == "STOP") {
+//         alert("OK, your final total is " + total);
+//         break;
+//     } else {
+//         priceOfItem = parseFloat(userInput);
+//         total = total + priceOfItem;
+//         alert("Your total cost is now " + total.toFixed(2));
+//     }
+// }
+
+
+// var number = Math.ceil(Math.random() * 6);
+// var guess;
+// do {
+//     guess =
+// }
