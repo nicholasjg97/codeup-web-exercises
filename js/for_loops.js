3e1a3f29b21fa4bc9a89d95867eb4d244f53dcec
Nicholas Gaytan <nicholas.j.gaytan97@gmail.com>
2021-07-15 12:06:15 -0500

    fixed first exercise of for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,21 @@
 'use strict'
 
-function showMultiplicationTable(number) {
-    for (i = 1; i <= 10; i++)
-        console.log(input * i)
+function showMultiplicationTable(input) {
+    for (let i = 1; i < 11; i++) {
+        console.log(input * i);
+    }
 }
 
 
+// var i = Math.floor(Math.random() * 200) + 20;
+// for (i > 0; i < 11; i++) {
+//     if (i % 2 === 0) {
+//         console.log("The number " + i + " is even.")
+//     } else {
+//         console.log("The number " + i + " is odd.")
+//     }
+// }
+
+// for (number) {
+//
+// }
