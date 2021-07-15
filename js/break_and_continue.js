1a200645dc65a621d006776a3f9286ccc566ea4c
Nicholas Gaytan <nicholas.j.gaytan97@gmail.com>
2021-07-15 17:17:35 -0500

    finished break and continue for loops exercises

---
+++ js/break_and_continue.js
@@ -0,0 +1,39 @@
+// var breakNumber = "";
+// for (i = 1; 1 < 50; i+=2) {
+//     var chooseNumber = prompt("Enter an odd number between 1 and 50!");
+//     if (chooseNumber % 2 === 0) {
+//         alert("That is not an odd number silly!");
+//         console.log("That is not an odd number silly!")
+//     }
+//     if (chooseNumber > 50) {
+//         alert("That's over 50! choose something under.")
+//         console.log("That's over 50! choose something under.")
+//     }
+//     if (chooseNumber === "31") {
+//         console.log("Yikes! You entered a bad number.");
+//         alert("Yikes! You entered a bad number.");
+//         break;
+//     }
+//     breakNumber = breakNumber + " " + chooseNumber;
+//     console.log("You chose the number: " + chooseNumber);
+// }
+
+// var breakNumber = "";
+// for (i = 1; 1 < 50; i+=2) {
+//     var chooseNumber = prompt("Enter an odd number between 1 and 50!");
+//     if (chooseNumber % 2 === 0) {
+//         alert("That is not an odd number silly!");
+//         console.log("That is not an odd number silly!")
+//     }
+//     if (chooseNumber > 50) {
+//         alert("That's over 50! choose something under.")
+//         console.log("That's over 50! choose something under.")
+//     }
+//     if (chooseNumber === "31") {
+//         console.log("Yikes! You entered a bad number.");
+//         alert("Yikes! You entered a bad number.");
+//         continue;
+//     }
+//     breakNumber = breakNumber + " " + chooseNumber;
+//     console.log("You chose the number: " + chooseNumber);
+// }
