1a5cd63407669b5c6e5a55e2a1bfd7a173359b0b
Nicholas Gaytan <nicholas.j.gaytan97@gmail.com>
2021-07-20 16:24:59 -0500

    renamed files to match with notes and exercises

--- js/external.js
+++ js/external.js
@@ -1,26 +1,47 @@
-"use strict";
-
-// alert("Hi, I am a Javascript alert!");
-
-// confirm
-
-// var userDecision = confirm("Are you sure you don't want to buy stuff on sale!")
-// console.log(userDecision);
+// console.log("Hello from external JavaScript");
 //
-// if (userDecision === true) {
-//     alert("Wise choice!");
-// } else {
-//     alert("Oh no, your loss!");
-// }
+// alert("Welcome to my Website");
+// var userColor = prompt("What is your favorite color?")
+// console.log(userColor);
+// alert("Nice, " + userColor + " is my favorite color too!");
 
-// prompt
 
-var currentSpeedInMPH = 65;
-var plannedSpeedInMPH = prompt("How fast do you plan to go in mph");
-var distanceLeftInMiles = prompt("How many miles away are you from your destinaiton?")
-var timeToDestinationArCurrentSpeed = (distanceLeftInMiles/currentSpeedInMPH) * 60;
-var timeToDestinationAtPlannedSpeed = (distanceLeftInMiles/plannedSpeedInMPH) * 60;
-var timeGain = timeToDestinationArCurrentSpeed - timeToDestinationAtPlannedSpeed;
-var minutesGained = Math.floor(timeGain);
-var seconds = Math.round((timeGain - minutesGained) * 60);
-console.log("If you drive at " + plannedSpeedInMPH + " mph, you will arrive " + minutesGained + " minutes and " + seconds + " seconds earlier");
+// var mermaidDays = prompt("How many days will you be watching The Little Mermaid?");
+// var brotherBearDays = prompt("How many days will you be watching Brother Bear?");
+// var herculesDays = prompt("How many days will you be watching Hercules?");
+// var rentalPerDayDollars = prompt("How much is it to rent a movie per day?")
+// var totalRentalCost = parseFloat(mermaidDays * rentalPerDayDollars) + parseFloat(brotherBearDays * rentalPerDayDollars) + parseFloat(herculesDays * rentalPerDayDollars)
+// alert("Nice, your total cost for all movies will be $" + totalRentalCost.toFixed(2));
+// console.log(totalRentalCost);
+
+
+// var googleHourlyRateDollars = prompt("How much does Google pay you?")
+// var amazonHourlyRateDollars = prompt("How much does Amazon pay you?");
+// var facebookHourlyRateDollars = prompt("How much does Facebook pay you?")
+// var googleHours = prompt("How many hours did you work for Google this week?");
+// var amazonHours = prompt("How many hours did you work for Amazon this week?");
+// var facebookHours = prompt("How many hours did you work for Facebook this week?");
+// var googlePayRate = googleHourlyRateDollars * googleHours;
+// var amazonPayRate = amazonHourlyRateDollars * amazonHours;
+// var facebookPayRate = facebookHourlyRateDollars * facebookHours;
+// var totalPayment = parseFloat(googlePayRate) + parseFloat(amazonPayRate) + parseFloat(facebookPayRate);
+// alert("You got paid $" + totalPayment.toFixed(2) + " this week!")
+// console.log(totalPayment);
+
+
+// var chooseClass = prompt("Choose a class to enroll in");
+// var classIsNotFull = confirm("PLease confirm class in not full");
+// var chooseTime = prompt("Enter the desired class you'd prefer to have")
+// var classScheduleCheck = confirm("Please confirm your new class works with your schedule");
+//
+// var studentEnrolled = classIsNotFull && classScheduleCheck;
+// alert("Student enrollment status: " + studentEnrolled);
+//
+//
+// var discountItemsOffer = 2;
+// var itemsBought = prompt("How many items were bought?");
+// var offerNotExpired = confirm("Please confirm offer is not expired");
+// var premiumMemberCheck = confirm("Please confirm you are a premium member");
+// var productDiscountApplied = offerNotExpired && (premiumMemberCheck || itemsBought >= discountItemsOffer);
+//
+// alert("The discount offer is applied: " + productDiscountApplied);
